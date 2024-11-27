b537177fb1cea421a2929b4c3f2b0814a15a6f20
eriri07 <eriri07@naver.com>
2024-11-27 14:31:41 +0900

    [Feat] 조합 시스템 & UI

--- Assets/Scripts/Building/BuildingDetector.cs
+++ Assets/Scripts/Building/BuildingDetector.cs
@@ -6,8 +6,10 @@ public class BuildingDetector : MonoBehaviour
 {
     public float checkRadius = 3.0f;            //아이템 감지 범위
     public Vector3 lastPosition;                //플레이어의 마지막 위치 (플레이어 이동이 감지 될 경우 주변을 찾기 위한 변수)
-    public float moveThreshold;                 //이동 감지 임계값
+    public float moveThreshold = 0.1f;                 //이동 감지 임계값
     public ConstructibleBuilding currentNearbyBuilding;   //가장 가까이 있는 수집 가능한 아이템
+    public BuildingCrafter currentBuildingCrafter;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,10 +27,18 @@ public class BuildingDetector : MonoBehaviour
             lastPosition = transform.position;      //현재 위치를 마지막 위치로 어베이트
         }
 
-        //가장 가까운 아이템이 있고 E키를 눌렀을 때 아이템 수집
+        //가장 가까운 건물이 있고 F키를 눌렀을 때 건물 건설
         if (currentNearbyBuilding != null && Input.GetKeyDown(KeyCode.F))
         {
-            currentNearbyBuilding.StartConstruction(GetComponent<PlayerInventory>());     //PlayerInventory 참조하여 아이템 수집
+            if (!currentNearbyBuilding.isConstruced)
+            {
+                currentNearbyBuilding.StartConstruction(GetComponent<PlayerInventory>());     //PlayerInventory 참조하여 아이템 수집
+            }
+            else if (currentBuildingCrafter != null)
+            {
+                Debug.Log($"{currentNearbyBuilding} 의 제작 메뉴 열기");
+                CraftingUIManager.Instance?.ShowUI(currentBuildingCrafter);
+            }
         }
     }
 
@@ -37,26 +47,30 @@ public class BuildingDetector : MonoBehaviour
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, checkRadius);           //감지 범위 내의 모든 콜라이더를 찾아옴
 
         float closestDistance = float.MaxValue;                  //가장 가까운 거리의 초기값
-        ConstructibleBuilding closestBuilding = null;                     //가장 가까운 아이템 초기값
+        ConstructibleBuilding closestBuilding = null;                     //가장 가까운 건물 초기값
+        BuildingCrafter closesCrafter = null;
 
-        foreach (Collider collider in hitColliders)                                                 //각 콜라이더를 검사하여 수집 가능한 아이템을 찾음
+        foreach (Collider collider in hitColliders)                                                 //각 콜라이더를 검사하여 수집 가능한 건물을 찾음
         {
-            ConstructibleBuilding building = collider.GetComponent<ConstructibleBuilding>();           //아이템을 감지
-            if (building != null && building.canBuild && !building.isConstruced)        //아이템이 있고 수집 가능한지 확인
+            ConstructibleBuilding building = collider.GetComponent<ConstructibleBuilding>();           //건물을 감지
+            if (building != null)        //건물이 있고 수집 가능한지 확인
             {
                 float distance = Vector3.Distance(transform.position, building.transform.position);     //거리 계산
                 if (distance < closestDistance)                                                     //더 가까운 아이템을 발견 시 업데이트
                 {
                     closestDistance = distance;
                     closestBuilding = building;
+                    closesCrafter = building.GetComponent<BuildingCrafter>();
                 }
             }
         }
 
-        if (closestBuilding != currentNearbyBuilding)           //가장 가까운 아이템이 변경 되었을 때 메세지 표시
+        if (closestBuilding != currentNearbyBuilding)           //가장 가까운 건물이 변경 되었을 때 메세지 표시
         {
-            currentNearbyBuilding = closestBuilding;            //가장 가까운 아이템 업데이트
-            if (currentNearbyBuilding != null)
+            currentNearbyBuilding = closestBuilding;            //가장 가까운 건물 업데이트
+            currentBuildingCrafter = closesCrafter;
+
+            if (currentNearbyBuilding != null && !currentNearbyBuilding.isConstruced)
             {
                 if (FloatingTextManager.instance != null)
                 {
